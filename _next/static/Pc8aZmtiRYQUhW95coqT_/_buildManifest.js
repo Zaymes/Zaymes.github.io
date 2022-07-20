5c8c0a8d9004af3f337ae86b7c51ef9c6a12276b
Zaymes <zaymesshrestha@gmail.com>
2022-07-20 09:46:52 +0000

    Deploying to gh-pages from @ Zaymes/Zaymes.github.io@5cfbbd40fdec95af56ea2c9f1a8244ef73a479b2 🚀

---
+++ _next/static/Pc8aZmtiRYQUhW95coqT_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-88423b94e32f5cb6.js","static/chunks/pages/index-e694d8a17c93f37d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
